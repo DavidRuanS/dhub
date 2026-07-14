191cfdfa8042bb3384f2a3c0f746b6d3b36138cd
David Ruan <142277090+DavidRuanS@users.noreply.github.com>
2026-07-14 11:39:52 -0300

    Mount synchronized wallet enhancer

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import AuthGate from './AuthGate'
+import WalletEnhancer from './WalletEnhancer'
 import './styles.css'
 import './overrides.css'
 
@@ -11,5 +12,5 @@ if ('serviceWorker' in navigator) {
 }
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode><AuthGate /></StrictMode>,
+  <StrictMode><AuthGate /><WalletEnhancer /></StrictMode>,
 )
